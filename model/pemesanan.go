fe03017dd854bea2aa9d3fa923a3e3192659284b
ghaidafasya24 <ghaidafasya5@gmail.com>
2024-12-01 23:15:06 +0700

    refactor: standardize formatting in Pemesanan struct

--- model/pemesanan.go
+++ model/pemesanan.go
@@ -5,12 +5,11 @@ import (
 )
 
 type Pemesanan struct {
-	ID          	 primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Fullname    	 string             `json:"fullname" bson:"fullname"`
-	Email       	 string             `json:"email" bson:"email"`
-	PhoneNumber 	 string             `json:"phone_number" bson:"phone_number"`
-	Category    	 DesignCategory     `bson:"category" json:"category"`
-	DesignType       string        		`json:"design_type" bson:"design_type"`
-	OrderDescription string 	   		`json:"order_description" bson:"order_description"`
-	UploadReferences string 	   		`json:"upload_references" bson:"upload_references"`
+	ID               primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Fullname         string             `json:"fullname" bson:"fullname"`
+	Email            string             `json:"email" bson:"email"`
+	PhoneNumber      string             `json:"phone_number" bson:"phone_number"`
+	Category         DesignCategory     `bson:"category" json:"category"`
+	OrderDescription string             `json:"order_description" bson:"order_description"`
+	UploadReferences string             `json:"upload_references" bson:"upload_references"`
 }
