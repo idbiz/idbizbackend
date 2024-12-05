850d86984de1173e366e3d110f5467f43cb88b59
ghaidafasya24 <ghaidafasya5@gmail.com>
2024-12-05 13:06:05 +0700

    fix: update CORS configuration to allow local development origin

--- config/cors.go
+++ config/cors.go
@@ -9,7 +9,7 @@ var Origins = []string{
 	"https://www.bukupedia.co.id",
 	"https://naskah.bukupedia.co.id",
 	"https://bukupedia.co.id",
-}
+	"http://127.0.0.1:5500"}
 
 // Fungsi untuk memeriksa apakah origin diizinkan
 func isAllowedOrigin(origin string) bool {
